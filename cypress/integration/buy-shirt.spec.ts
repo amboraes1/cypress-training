e131120727018aab41075b17bdabcadc2e12d4aa
Tomas ALvarez <tomas.alvarez@perficient.com>
2022-04-01 14:33:45 -0500

    Adding automation script finished

---
+++ cypress/integration/buy-shirt.spec.ts
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+
+describe("Buy a t-shirt", () => {
+  it.only("then the t-shirt should be bought", () => {
+    cy.visit("http://automationpractice.com/");
+    cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
+    cy.get("#center_column a.button.ajax_add_to_cart_button.btn.btn-default").click();
+    cy.get("[style*='display: block;'] .button-container > a").click();
+    cy.get(".cart_navigation span").click();
+
+    cy.get("#email").type("tomas.alvarez@perficient.com");
+    cy.get("#passwd").type("test123");
+
+    
+    // Debes completar la prueba ... implementa los pasos 8 al 13, del proceso de compra
+    cy.get("#SubmitLogin").click();
+    cy.get("button[name='processAddress']").click();
+
+    cy.get("#cgv").click();
+    cy.get("button[name='processCarrier']").click();
+
+    cy.get(".bankwire").click();
+
+    cy.contains("span","I confirm my order").click();
+
+    cy.get("#center_column > div > p > strong")
+      .should("have.text", "Your order on My Store is complete.");
+  });
+});
