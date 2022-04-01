03d8947c89227dc45b0e20d6fcf6b5907cafcffa
Tomas ALvarez <tomas.alvarez@perficient.com>
2022-04-01 12:14:32 -0500

    slint, travis set up

---
+++ cypress/integration/google.spec.ts
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+describe("This is my first cypress test", () => {
+  it("should have a title", () => {
+    cy.visit("https://www.google.com/");
+    cy.title().should("eq", "Google");
+  });
+});
