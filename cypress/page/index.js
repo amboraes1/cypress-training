4926448da3db75b44c102a6a128a5cb25b1ceec8
Tomas ALvarez <tomas.alvarez@perficient.com>
2022-08-02 14:13:13 -0500

    adding changes

--- cypress/page/index.js
+++ cypress/page/index.js
@@ -1,9 +1,20 @@
+// eslint-disable-next-line import/no-unresolved
 export { MenuContentPage } from "./menu-content.page";
+// eslint-disable-next-line import/no-unresolved
 export { PLPPage } from "./plp.page";
+// eslint-disable-next-line import/no-unresolved
 export { CartPage } from "./cart.page";
+// eslint-disable-next-line import/no-unresolved
 export { SigninPage } from "./signin.page";
+// eslint-disable-next-line import/no-unresolved
 export { AddressesPage } from "./addresses.page";
+// eslint-disable-next-line import/no-unresolved
 export { ShippingPage } from "./shipping.page";
+// eslint-disable-next-line import/no-unresolved
 export { PaymentPage } from "./payment.page";
+// eslint-disable-next-line import/no-unresolved
 export { ConfirmPaymentPage } from "./confirmpayment.page";
+// eslint-disable-next-line import/no-unresolved
 export { OrderConfirmationPage } from "./orderconfirmation.page";
+// eslint-disable-next-line import/no-unresolved
+export { DressesListPage } from "./dresseslist.page";
