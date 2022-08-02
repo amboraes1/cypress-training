eb13ce928d1b13e14618c329f775646d0040de3e
Tomas ALvarez <tomas.alvarez@perficient.com>
2022-08-02 15:11:45 -0500

    adding AAA solved

--- cypress/integration/dresses-list.spec.ts
+++ cypress/integration/dresses-list.spec.ts
@@ -1,19 +1,30 @@
 /// <reference types="Cypress" />
 
 // eslint-disable-next-line import/extensions
+// @ts-ignore
 import { MenuContentPage, DressesListPage } from "../page/index";
 
 describe("the user navigates to the dresses page should", () => {
   let menuContentPage: MenuContentPage;
   let dressesListPage: DressesListPage;
+  const dressesName:String[] = [];
 
   before(() => {
     menuContentPage = new MenuContentPage();
     dressesListPage = new DressesListPage();
+    dressesName.push("Printed Dress");
+    dressesName.push("Printed Dress");
+    dressesName.push("Printed Summer Dress");
+    dressesName.push("Printed Summer Dress");
+    dressesName.push("Printed Chiffon Dress");
+    menuContentPage.visitMenuContentPage();
   });
 
   it("show the available dresses", () => {
-    // ... realiza la prueba
+    menuContentPage.goToDressMenu();
 
+    dressesListPage.validateItemsNumber(dressesName.length);
+
+    dressesListPage.validateItemsNames(dressesName);
   });
 });
--- cypress/page/menu-content.page.ts
+++ cypress/page/menu-content.page.ts
@@ -1,10 +1,12 @@
 class MenuContentPage {
     private tShirtMenu: string;
     private menuContentPageURL: string;
+    private dressesMenu: string;
 
     constructor() {
         this.tShirtMenu = "#block_top_menu > ul > li:nth-child(3) > a";
         this.menuContentPageURL = "http://automationpractice.com/";
+        this.dressesMenu = ".sf-menu > :nth-child(2) > .sf-with-ul";
     }
 
     public visitMenuContentPage(): void {
@@ -13,6 +15,10 @@ class MenuContentPage {
 
     public goToTShirtMenu(): void {
         cy.get(this.tShirtMenu).click();
+    }
+
+    public goToDressMenu(): void {
+        cy.get(this.dressesMenu).click();
     }
 
 }
